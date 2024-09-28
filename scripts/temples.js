1267f7b23230579c728c71d22877517f6a9a2dde
niels012 <urm21001@byui.edu>
2024-09-29 01:57:44 +0800

    nav edited

--- scripts/temples.js
+++ scripts/temples.js
@@ -6,14 +6,19 @@ document.getElementById('lastModified').textContent = "Last Modification: " + la
 
 
 
+// script.js
+
 const hamburger = document.getElementById('hamburger');
 const navLinks = document.getElementById('nav-links');
 
+// Toggle the visibility of navigation links
 hamburger.addEventListener('click', () => {
-  navLinks.classList.toggle('hidden');
-  if (hamburger.textContent === '\u2630') {
-    hamburger.textContent = 'X';
-  } else {
-    hamburger.textContent = '\u2630';
-  }
+    navLinks.classList.toggle('show-links');
+    
+    // Toggle the hamburger icon
+    if (hamburger.textContent === '\u2630') {
+        hamburger.textContent = 'X';
+    } else {
+        hamburger.textContent = '\u2630';
+    }
 });
