f5be03f54444731f14e59e218a4a96ce623aab64
niels012 <urm21001@byui.edu>
2024-10-29 02:40:33 +0800

    nav toggle modified

--- project/scripts/portfolio.js
+++ project/scripts/portfolio.js
@@ -4,11 +4,12 @@ const navLinks = document.getElementById('nav-links');
 // Toggle the visibility of navigation links
 hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('show-links');
-    
+
     // Toggle the hamburger icon
     if (hamburger.textContent === '\u2630') {
         hamburger.textContent = 'X';
     } else {
         hamburger.textContent = '\u2630';
     }
+
 });
